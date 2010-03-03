aa9ea1dd92f61cd7bcbb675ca4938b20f9efafb8
Maxim Dobriakov <closer.main@gmail.com>
2010-03-03 10:12:59 +0300

    Added validation for feature model and change default validation messages

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,6 +2,10 @@ class Feature < ActiveRecord::Base
 
   belongs_to :idea
 
+  validates_presence_of :title
+  validates_presence_of :weight
+  validates_inclusion_of :weight, :in => -1000..1000
+
   def to_param
     if new_record?
       super
