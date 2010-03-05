1c5c78d0c816182441041450fbc02c4eab9d15b7
Maxim Dobriakov <closer.main@gmail.com>
2010-03-06 01:38:52 +0300

    Fix JS bug for Opera related to CSS color format

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,12 @@
 function rgb2hex(str) {
-    var rgb = str.match(/\d+/g);
+    if (str[0] == "#")
+        return str;
 
     function hex(x) {
         hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8","9", "A", "B", "C", "D", "E", "F");
         return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
     }
+    
+    var rgb = str.match(/\d+/g);
     return "#" + hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);
 }
